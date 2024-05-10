2a3ff78fbc066f59150911071ee1618eb577320d
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-05-10 01:15:54 -0500

    Fix credits gen to use actual API

--- scripts/auto_credits.py
+++ scripts/auto_credits.py
@@ -19,8 +19,8 @@ for mod in mods_list:
     with open("../plus/mods/" + mod, "rb") as mod_file:
         mod_file_data = tomllib.load(mod_file)
         # Gets data from Modrinth
-        mod_meta_data = json.loads(requests.get("https://staging-api.modrinth.com/v2/project/" + mod_file_data.get("update").get("modrinth").get("mod-id")).text)
-        mod_user_data = json.loads(requests.get("https://staging-api.modrinth.com/v2/project/" + mod_file_data.get("update").get("modrinth").get("mod-id") + "/members").text)
+        mod_meta_data = json.loads(requests.get("https://api.modrinth.com/v2/project/" + mod_file_data.get("update").get("modrinth").get("mod-id")).text)
+        mod_user_data = json.loads(requests.get("https://api.modrinth.com/v2/project/" + mod_file_data.get("update").get("modrinth").get("mod-id") + "/members").text)
         print("Generating credits for " + mod_meta_data.get("title") + " by " + mod_user_data[-1].get("user").get("username") + "...")
         mod_credits = mod_credits + "- [" + str(mod_meta_data.get("title")) + "](https://modrinth.com/mod/" + str(mod_meta_data.get("slug")) + ") by [" + str(mod_user_data[-1].get("user").get("username")) + "](https://modrinth.com/user/" + str(mod_user_data[-1].get("user").get("username")) + ") (" + str(mod_meta_data.get("license").get("name")) + ")\n"
 
