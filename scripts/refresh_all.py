6d562f7242490a579b10ee1a1ea44e680a0ed475
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-09-13 17:41:51 -0500

    Fixed the script

--- scripts/refresh_all.py
+++ scripts/refresh_all.py
@@ -2,6 +2,6 @@
 import subprocess
 
 # Run the refresh command for all projects
-subprocess.call(["cd", "../lite", "&&", "packwiz", "refresh"], shell=True) 
-subprocess.call(["cd", "../normal", "&&", "packwiz", "refresh"], shell=True)
-subprocess.call(["cd", "../plus", "&&", "packwiz", "refresh"], shell=True)
+subprocess.call(["cd", "./lite", "&&", "packwiz", "refresh"], shell=True) 
+subprocess.call(["cd", "./normal", "&&", "packwiz", "refresh"], shell=True)
+subprocess.call(["cd", "./plus", "&&", "packwiz", "refresh"], shell=True)
