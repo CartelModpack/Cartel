28cb5940ab0b656abc3934b59071a3317074184d
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-12-20 16:12:33 -0600

    Added command line args to script

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,38 @@
+import { getRequest } from "./api/api.js";
 import args from "./installer/getDesiredVersion.js";
+import CartelAPIResponse, {
+  CartelAPILatest,
+} from "./interfaces/api/CartelAPI.js";
 import logger from "./logger.js";
 
-logger.info(JSON.stringify(args, null, 2));
-
-/*
-function printError(from: string) {
-  return (error) => logger.error(`${from} > ${new Error(error)}`);
+function printError(errorFrom: string) {
+  return (error: any) => logger.error(`${errorFrom} > ${error}`);
 }
 
-logger.info("Booting Cartel Updater...");
+logger.info("Starting up Cartel Updater...");
+
+// Get the latest versions of cartel, fabric, mc, etc.
 getRequest("server", "/api/v1/latest")
-  .then((data: CartelAPIResponse<CartelAPILatest>) => {
-    if (data.status == 200) {
-      let { content } = data;
-      logger.info("Latest Versions: " + JSON.stringify(content, null, 0));
-      getRequest(
-        "fabric",
-        `/v2/versions/loader/${content.game}/${content.loader}/profile/json`
-      )
-        .then((fabricLoaderData: any) => {
-          logger.info(
-            `Would use "${fabricLoaderData.id}" as a base, renamed to "Cartel v${content.cartel} [${content.game}/4Fabric ${content.loader}]"`
-          );
-        })
-        .catch(printError("Fabric Request"));
+  .then((cartelLatest: CartelAPIResponse<CartelAPILatest>) => {
+    if (cartelLatest.status == 200) {
+      // Get the install options we are using.
+      let options = {
+        minecraft:
+          args.minecraft == "latest" || args.minecraft == null
+            ? cartelLatest.content.game
+            : args.minecraft,
+        fabric:
+          args.fabric == "latest" || args.fabric == null
+            ? cartelLatest.content.loader
+            : args.fabric,
+      };
+
+      // Let the user know whats up...
+      logger.info(
+        `Installing Cartel v${cartelLatest.content.cartel} [${options.minecraft}/Fabric ${options.fabric}]`
+      );
     } else {
-      logger.warn(`Status Code ${data.status}: ${data.error}`);
+      printError("Cartel Server")(cartelLatest.error);
     }
   })
-  .catch(printError("Cartel Server Request"));
-
-getRequest(
-  "fabric",
-  "/v2/versions/loader/:game_version/:loader_version/profile/json",
-  (error, response: FabricMetaAPIVersions) => {
-    if (error) {
-      logger.error(error);
-    } else {
-      downloadFile(
-        response.installer[0].url,
-        null,
-        "fabric-installer.jar",
-        "game",
-        (err) => {
-          if (err) {
-            logger.error(err);
-          } else {
-            let user = join(process.cwd(), "/game/mc");
-            logger.info(user);
-            let cmd = `java -jar ./game/fabric-installer.jar client -noprofile -dir "${user}"`;
-            let cp = exec(cmd);
-            cp.on("message", (msg) => {
-              logger.error(msg.toString());
-            });
-            cp.on("exit", (code) => {
-              logger.info("Fabric Installer exited with code " + code);
-            });
-          }
-          // java -jar fabric-installer.jar client -dir "~/Games/.minecraft"
-        }
-      );
-    }
-  }
-);
-*/
+  .catch(printError("Cartel Server"));
